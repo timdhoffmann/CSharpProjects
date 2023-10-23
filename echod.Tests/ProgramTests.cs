d4f5e873da02f06c026718e072a545500b4d54d9
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2023-10-23 18:24:42 +0200

    Add integration test for one arg.

--- echod.Tests/ProgramTests.cs
+++ echod.Tests/ProgramTests.cs
@@ -1,9 +1,20 @@
+using Xunit.Abstractions;
+
 namespace echod.Tests;
 
 public class ProgramTests
 {
+
+    public ProgramTests(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+
+    // Helper for debug logging.
+    private readonly ITestOutputHelper output;
+
     [Fact]
-    public void Main_WithoutArgs_ReturnsMissingArgsError()
+    public void Main_WithoutArgs_StdErrContainsMissingArgs()
     {
         var args = Array.Empty<string>();
         const string expectedOutput = "Required argument missing for command";
@@ -12,6 +23,24 @@ public class ProgramTests
 
         Program.Main(args);
         var actualOutput = stringWriter.ToString();
+
+        Assert.Contains(expectedOutput, actualOutput);
+    }
+
+    [Fact]
+    public void Main_WithSomeArg_StdOutContainsArg()
+    {
+        var args = new [] { "hello" };
+        const string expectedOutput = "hello";
+
+        using var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+
+        Program.Main(args);
+        var actualOutput = stringWriter.ToString();
+        // To see this being printed to the console, run the test with
+        // 'dotnet test --logger "console;verbosity=detailed"'
+        output.WriteLine($"[DEBUG] {actualOutput}");
 
         Assert.Contains(expectedOutput, actualOutput);
     }
